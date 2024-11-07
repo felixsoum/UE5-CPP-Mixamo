61043aa8b222a920d58e5beca81aa0dc1091fce2
Felix Soumpholphakdy <felix@felixsoum.com>
2024-11-07 09:51:23 -0500

    Npc branch

---
+++ Source/UE5CppMixamo/Private/NPCCharacter.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "NPCCharacter.generated.h"
+
+UCLASS()
+class ANPCCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ANPCCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual void NotifyHit(class UPrimitiveComponent* MyComp, AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
+	
+	UPROPERTY(EditAnywhere)	
+	TObjectPtr<UAnimMontage> HitAnimMontage;
+};
